d49628c5df0d533b0d4b5fe79169e7ea6228f7dd
salihabrik <briksaliha@gmail.com>
2024-02-03 22:31:54 +0100

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+const api_url = "https://api.quotable.io/random" 
+
+
+async function getQuote(url) {
+    const response = await fetch(url)
+    var data = await response.json()
+    console.log(data)
+}
+
+
+getQuote(api_url);
