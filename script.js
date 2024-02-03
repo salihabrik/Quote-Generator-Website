64463cc99fb6ea524cff12caa09be35b247302c3
salihabrik <briksaliha@gmail.com>
2024-02-03 22:41:48 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const api_url = "https://api.quotable.io/random" 
+const quote = d
 
+
+const api_url = "https://api.quotable.io/random";
 
 async function getQuote(url) {
-    const response = await fetch(url)
-    var data = await response.json()
-    console.log(data)
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 }
-
 
 getQuote(api_url);
